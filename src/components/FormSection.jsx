bba164b410141bc7f06bfd72af2c63fc7cfd4381
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-05-08 11:01:54 -0300

    Create the "Form section" component

---
+++ src/components/FormSection.jsx
@@ -0,0 +1,13 @@
+import { GralInfo } from "./GralInfo.jsx";
+import { EdExp } from "./EdExp.jsx";
+import { PracticalExp } from "./PracticalExp.jsx";
+export function FormSection() {
+  return (
+    <>
+      <h1>CV Generator</h1>
+      <GralInfo></GralInfo>
+      <EdExp></EdExp>
+      <PracticalExp></PracticalExp>
+    </>
+  );
+}
