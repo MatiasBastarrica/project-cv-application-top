ff2abbe4d20098c8ace0e12e0e0416168929dd77
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-05-08 11:00:20 -0300

    Create the "Education experience" component

---
+++ src/components/EdExp.jsx
@@ -0,0 +1,23 @@
+export function EdExp() {
+  return (
+    <div className="cv-form-field">
+      <h2>Educational experience</h2>
+      <form action="#">
+        <div>
+          <label htmlFor="school-name">School name</label>
+          <input type="text" id="school-name" />
+        </div>
+        <div>
+          <label htmlFor="title-of-study">Title of study</label>
+          <input type="text" name="title-of-study" id="title-of-study" />
+        </div>
+        <div>
+          <label htmlFor="ed-start-date">Start date</label>
+          <input type="date" name="ed-start-date" id="ed-start-date" />
+          <label htmlFor="ed-end-date">End date</label>
+          <input type="date" name="ed-end-date" id="ed-end-date" />
+        </div>
+      </form>
+    </div>
+  );
+}
