794ea9ebc6dfc386d537c1ebffed5561f730cb84
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-05-08 19:43:28 -0300

    Create and style the "Accordion" component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { FormSection } from "../src/components/FormSection.jsx";
+// import { FormSection } from "../src/components/FormSection.jsx";
+import { Accordion } from "./components/Accordion.jsx";
 // example of how to import assets
 // import reactLogo from './assets/react.svg'
 
@@ -8,7 +9,16 @@ import "./App.css";
 function App() {
   return (
     <>
-      <FormSection></FormSection>
+      <Accordion title={"Accordion title"}>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
+          officiis accusamus esse dolore voluptatem atque perferendis delectus
+          nesciunt. Exercitationem, quibusdam neque corporis veniam, cumque, ab
+          ullam perferendis obcaecati nisi porro vitae nulla corrupti blanditiis
+          sapiente pariatur ratione mollitia! Sunt inventore sequi earum facilis
+          unde in, alias beatae quis maxime amet.
+        </p>
+      </Accordion>
     </>
   );
 }
---
+++ src/components/Accordion.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export function Accordion({ children, title }) {
+  const [openAccordion, setOpenAccordion] = useState(false);
+
+  function handleAccordion(e) {
+    const btn = e.currentTarget;
+    const accordionContent = document.querySelector(".accordion-content");
+    accordionContent.classList.toggle("hide");
+    if (!openAccordion) {
+      btn.classList.remove("accordion-closed");
+      btn.classList.add("accordion-opened");
+    } else {
+      btn.classList.add("accordion-closed");
+      btn.classList.remove("accordion-opened");
+    }
+    setOpenAccordion(!openAccordion);
+  }
+  return (
+    <div className="accordion-container">
+      <div className="accordion-title">
+        <h2>{title}</h2>
+        <button
+          type="button"
+          className="accordion-closed"
+          onClick={handleAccordion}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>Expand form field</title>
+            <path d="M5.5,4.14L4.5,5.86L15,12L4.5,18.14L5.5,19.86L19,12L5.5,4.14Z" />
+          </svg>
+        </button>
+      </div>
+      <div className="accordion-content">{children}</div>
+    </div>
+  );
+}
