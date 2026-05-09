f85f065933879ffe7fb629158291edf0b391df19
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-05-09 15:59:30 -0300

    Position and style the "edit" button

--- src/components/GralInfo.jsx
+++ src/components/GralInfo.jsx
@@ -9,17 +9,11 @@ export function GralInfo() {
     phone: "",
   });
 
-  // function toggleForm() {
-  //   const formElement = document.querySelector(".gral-info-form");
-  //   formElement.classList.toggle("hide");
-  // }
-
   function handleSubmit(e) {
     e.preventDefault();
     submission.name = document.querySelector("#name").value;
     submission.email = document.querySelector("#email").value;
     submission.phone = document.querySelector("#phone").value;
-    // toggleForm();
     setSubmission({ ...submission, submitted: true });
     console.log(submission);
   }
@@ -61,14 +55,14 @@ export function GralInfo() {
       );
     } else {
       return (
-        <>
+        <div className="form-values-display">
           <p>Name: {submission.name}</p>
           <p>Email: {submission.email}</p>
           <p>Phone: {submission.phone}</p>
           <button type="button" className="edit-button" onClick={handleEdit}>
             Edit
           </button>
-        </>
+        </div>
       );
     }
   }
@@ -76,41 +70,4 @@ export function GralInfo() {
   return (
     <Accordion title={"General Information"}>{childrenElements()}</Accordion>
   );
-
-  // if (!submitted) {
-  //   return (
-  //     <div className="cv-form-field">
-  //       <div className="field-title">
-  //         <h2>General information </h2>
-  //         <button
-  //           type="button"
-  //           className="accordion-closed"
-  //           onClick={handleAccordion}
-  //         >
-  //           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-  //             <title>Expand form field</title>
-  //             <path d="M5.5,4.14L4.5,5.86L15,12L4.5,18.14L5.5,19.86L19,12L5.5,4.14Z" />
-  //           </svg>
-  //         </button>
-  //       </div>
-  //       <form action="#" className="hide gral-info-form">
-  //         <div>
-  //           <label htmlFor="name">Name</label>
-  //           <input type="text" id="name" />
-  //         </div>
-  //         <div>
-  //           <label htmlFor="email">Email</label>
-  //           <input type="email" name="email" id="email" />
-  //         </div>
-  //         <div>
-  //           <label htmlFor="phone">Phone</label>
-  //           <input type="number" name="phone" id="phone" />
-  //         </div>
-  //         <button type="submit" onClick={handleSubmit}>
-  //           Submit
-  //         </button>
-  //       </form>
-  //     </div>
-  //   );
-  // }
 }
