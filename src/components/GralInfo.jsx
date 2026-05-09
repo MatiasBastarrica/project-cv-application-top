4220f8f8628c51b9334ce94cbb153ec1ac6047f0
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-05-09 15:55:18 -0300

    Add an "edit" button after the form submission

--- src/components/GralInfo.jsx
+++ src/components/GralInfo.jsx
@@ -1,75 +1,116 @@
 import { useState } from "react";
+import { Accordion } from "./Accordion.jsx";
 
 export function GralInfo() {
-  const [openAccordion, setOpenAccordion] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
-
-  const submittedInfo = {
+  const [submission, setSubmission] = useState({
+    submitted: false,
     name: "",
     email: "",
     phone: "",
-  };
+  });
 
-  function toggleForm() {
-    const formElement = document.querySelector(".gral-info-form");
-    formElement.classList.toggle("hide");
-  }
-
-  function handleAccordion(e) {
-    const btn = e.currentTarget;
-    toggleForm();
-    if (!openAccordion) {
-      btn.classList.remove("accordion-closed");
-      btn.classList.add("accordion-opened");
-    } else {
-      btn.classList.add("accordion-closed");
-      btn.classList.remove("accordion-opened");
-    }
-    setOpenAccordion(!openAccordion);
-  }
+  // function toggleForm() {
+  //   const formElement = document.querySelector(".gral-info-form");
+  //   formElement.classList.toggle("hide");
+  // }
 
   function handleSubmit(e) {
     e.preventDefault();
-    submittedInfo.name = document.querySelector("#name").value;
-    submittedInfo.email = document.querySelector("#email").value;
-    submittedInfo.phone = document.querySelector("#phone").value;
-    toggleForm();
+    submission.name = document.querySelector("#name").value;
+    submission.email = document.querySelector("#email").value;
+    submission.phone = document.querySelector("#phone").value;
+    // toggleForm();
+    setSubmission({ ...submission, submitted: true });
+    console.log(submission);
   }
 
-  if (!submitted) {
-    return (
-      <div className="cv-form-field">
-        <div className="field-title">
-          <h2>General information </h2>
-          <button
-            type="button"
-            className="accordion-closed"
-            onClick={handleAccordion}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <title>Expand form field</title>
-              <path d="M5.5,4.14L4.5,5.86L15,12L4.5,18.14L5.5,19.86L19,12L5.5,4.14Z" />
-            </svg>
-          </button>
-        </div>
-        <form action="#" className="hide gral-info-form">
+  function handleEdit(e) {
+    setSubmission({ ...submission, submitted: false });
+  }
+
+  function childrenElements() {
+    if (!submission.submitted) {
+      return (
+        <form action="#" className=" gral-info-form">
           <div>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" />
+            <input type="text" id="name" defaultValue={submission.name} />
           </div>
           <div>
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              defaultValue={submission.email}
+            />
           </div>
           <div>
             <label htmlFor="phone">Phone</label>
-            <input type="number" name="phone" id="phone" />
+            <input
+              type="number"
+              name="phone"
+              id="phone"
+              defaultValue={submission.phone}
+            />
           </div>
           <button type="submit" onClick={handleSubmit}>
             Submit
           </button>
         </form>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <>
+          <p>Name: {submission.name}</p>
+          <p>Email: {submission.email}</p>
+          <p>Phone: {submission.phone}</p>
+          <button type="button" className="edit-button" onClick={handleEdit}>
+            Edit
+          </button>
+        </>
+      );
+    }
   }
+
+  return (
+    <Accordion title={"General Information"}>{childrenElements()}</Accordion>
+  );
+
+  // if (!submitted) {
+  //   return (
+  //     <div className="cv-form-field">
+  //       <div className="field-title">
+  //         <h2>General information </h2>
+  //         <button
+  //           type="button"
+  //           className="accordion-closed"
+  //           onClick={handleAccordion}
+  //         >
+  //           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  //             <title>Expand form field</title>
+  //             <path d="M5.5,4.14L4.5,5.86L15,12L4.5,18.14L5.5,19.86L19,12L5.5,4.14Z" />
+  //           </svg>
+  //         </button>
+  //       </div>
+  //       <form action="#" className="hide gral-info-form">
+  //         <div>
+  //           <label htmlFor="name">Name</label>
+  //           <input type="text" id="name" />
+  //         </div>
+  //         <div>
+  //           <label htmlFor="email">Email</label>
+  //           <input type="email" name="email" id="email" />
+  //         </div>
+  //         <div>
+  //           <label htmlFor="phone">Phone</label>
+  //           <input type="number" name="phone" id="phone" />
+  //         </div>
+  //         <button type="submit" onClick={handleSubmit}>
+  //           Submit
+  //         </button>
+  //       </form>
+  //     </div>
+  //   );
+  // }
 }
