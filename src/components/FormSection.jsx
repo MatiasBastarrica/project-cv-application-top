dd34e5d45a75a39b0d787e926c6015fdc2b183f5
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-05-11 22:05:37 -0300

    Pass props to each child element
    These props will help to update the "preview
    section"

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -1,14 +1,23 @@
 import { GralInfo } from "./GralInfo.jsx";
 import { EdExp } from "./EdExp.jsx";
 import { PracticalExp } from "./PracticalExp.jsx";
-export function FormSection() {
+export function FormSection({ previewData, updatePreviewData }) {
   return (
     <>
       <div className="form-section">
         <h1>CV Generator</h1>
-        <GralInfo></GralInfo>
-        <EdExp></EdExp>
-        <PracticalExp></PracticalExp>
+        <GralInfo
+          previewData={previewData}
+          updatePreviewData={(updatedData) => updatePreviewData(updatedData)}
+        ></GralInfo>
+        <EdExp
+          previewData={previewData}
+          updatePreviewData={(updatedData) => updatePreviewData(updatedData)}
+        ></EdExp>
+        <PracticalExp
+          previewData={previewData}
+          updatePreviewData={(updatedData) => updatePreviewData(updatedData)}
+        ></PracticalExp>
       </div>
     </>
   );
