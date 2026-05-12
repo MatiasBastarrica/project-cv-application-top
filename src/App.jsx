ebe1f3287757a4f63b105d81af2ad2035b27b250
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-05-11 22:04:18 -0300

    Use "state" to display the data from each form to the "preview section"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FormSection } from "../src/components/FormSection.jsx";
+import { PreviewSection } from "./components/PreviewSection.jsx";
 // import { Accordion } from "./components/Accordion.jsx";
 // example of how to import assets
 // import reactLogo from './assets/react.svg'
@@ -7,9 +8,14 @@ import { FormSection } from "../src/components/FormSection.jsx";
 import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({});
   return (
     <>
-      <FormSection></FormSection>
+      <FormSection
+        previewData={formData}
+        updatePreviewData={(updatedData) => setFormData(updatedData)}
+      ></FormSection>
+      <PreviewSection info={formData}></PreviewSection>
     </>
   );
 }
