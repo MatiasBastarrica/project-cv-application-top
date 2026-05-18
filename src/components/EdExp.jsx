1e5a723033c4504decf0132c3add0981a299d03c
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-05-17 22:22:12 -0300

    Allow the user to add more items to the "Educational experience" field

--- src/components/EdExp.jsx
+++ src/components/EdExp.jsx
@@ -2,96 +2,153 @@ import { useState } from "react";
 import { Accordion } from "./Accordion.jsx";
 
 export function EdExp({ previewData, updatePreviewData }) {
-  const [submission, setSubmission] = useState({
-    submitted: false,
-    schoolName: "",
-    titleOfStudy: "",
-    startDate: "",
-    endDate: "",
-  });
+  const [items, setItems] = useState([createItem()]);
+
+  function createItem() {
+    const item = {
+      submitted: false,
+      schoolName: "",
+      titleOfStudy: "",
+      startDate: "",
+      endDate: "",
+    };
+    return item;
+  }
+
+  function ItemForm({ index }) {
+    return (
+      <form action="#" className=" ed-exp-form">
+        <div>
+          <label htmlFor="school-name">School name</label>
+          <input
+            type="text"
+            id="school-name"
+            defaultValue={items[index].schoolName}
+          />
+        </div>
+        <div>
+          <label htmlFor="title-of-study">Title of study</label>
+          <input
+            type="text"
+            name="title-of-study"
+            id="title-of-study"
+            defaultValue={items[index].titleOfStudy}
+          />
+        </div>
+        <div className="date-inputs-container">
+          <div>
+            <label htmlFor="ed-start-date">Start date</label>
+            <input
+              type="date"
+              name="ed-start-date"
+              id="ed-start-date"
+              defaultValue={items[index].startDate}
+            />
+          </div>
+          <div>
+            <label htmlFor="ed-end-date">End date</label>
+            <input
+              type="date"
+              name="ed-end-date"
+              id="ed-end-date"
+              defaultValue={items[index].endDate}
+            />
+          </div>
+        </div>
+        <button type="submit" data-item-count={index} onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+
+  function ItemPreview({ index }) {
+    return (
+      <div className="form-values-display">
+        <p>School name: {items[index].schoolName}</p>
+        <p>Title of study: {items[index].titleOfStudy}</p>
+        <p>Start date: {items[index].startDate}</p>
+        <p>End date: {items[index].endDate}</p>
+        <button
+          type="button"
+          data-item-count={index}
+          className="edit-button"
+          onClick={handleEdit}
+        >
+          Edit
+        </button>
+      </div>
+    );
+  }
+
+  function getUpdatedItems(e, isSubmitted, isNew, index) {
+    const newItems = [...items];
+    if (!isNew) {
+      const updatedItem = { ...items[index], submitted: isSubmitted };
+      newItems.splice(index, 1, updatedItem);
+    } else {
+      newItems.push(createItem());
+    }
+    return newItems;
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
-    submission.schoolName = document.querySelector("#school-name").value;
-    submission.titleOfStudy = document.querySelector("#title-of-study").value;
-    submission.startDate = document.querySelector("#ed-start-date").value;
-    submission.endDate = document.querySelector("#ed-end-date").value;
-    setSubmission({ ...submission, submitted: true });
-    updatePreviewData({
-      ...previewData,
-      schoolName: submission.schoolName,
-      titleOfStudy: submission.titleOfStudy,
-      edStartDate: submission.startDate,
-      edEndDate: submission.endDate,
-    });
+    const index = e.target.dataset.itemCount;
+    const newItems = getUpdatedItems(e, true, false, index);
+
+    newItems[index].schoolName = document.querySelector("#school-name").value;
+    newItems[index].titleOfStudy =
+      document.querySelector("#title-of-study").value;
+    newItems[index].startDate = document.querySelector("#ed-start-date").value;
+    newItems[index].endDate = document.querySelector("#ed-end-date").value;
+
+    setItems([...newItems]);
   }
 
   function handleEdit(e) {
-    setSubmission({ ...submission, submitted: false });
+    const index = e.target.dataset.itemCount;
+    const newItems = getUpdatedItems(e, false, false, index);
+    setItems([...newItems]);
   }
 
-  function childrenElements() {
-    if (!submission.submitted) {
+  function handleAddMore(e) {
+    const newItems = getUpdatedItems(e, null, true);
+    setItems([...newItems]);
+    // const
+  }
+
+  function getItem(item, keyNum) {
+    if (!item.submitted) {
       return (
-        <form action="#" className=" ed-exp-form">
-          <div>
-            <label htmlFor="school-name">School name</label>
-            <input
-              type="text"
-              id="school-name"
-              defaultValue={submission.schoolName}
-            />
-          </div>
-          <div>
-            <label htmlFor="title-of-study">Title of study</label>
-            <input
-              type="text"
-              name="title-of-study"
-              id="title-of-study"
-              defaultValue={submission.titleOfStudy}
-            />
-          </div>
-          <div className="date-inputs-container">
-            <div>
-              <label htmlFor="ed-start-date">Start date</label>
-              <input
-                type="date"
-                name="ed-start-date"
-                id="ed-start-date"
-                defaultValue={submission.startDate}
-              />
-            </div>
-            <div>
-              <label htmlFor="ed-end-date">End date</label>
-              <input
-                type="date"
-                name="ed-end-date"
-                id="ed-end-date"
-                defaultValue={submission.endDate}
-              />
-            </div>
-          </div>
-          <button type="submit" onClick={handleSubmit}>
-            Submit
-          </button>
-        </form>
+        <div key={keyNum}>
+          <ItemForm index={keyNum}></ItemForm>
+        </div>
       );
     } else {
       return (
-        <div className="form-values-display">
-          <p>School name: {submission.schoolName}</p>
-          <p>Title of study: {submission.titleOfStudy}</p>
-          <p>Start date: {submission.startDate}</p>
-          <p>End date: {submission.endDate}</p>
-          <button type="button" className="edit-button" onClick={handleEdit}>
-            Edit
-          </button>
+        <div key={keyNum}>
+          <ItemPreview index={keyNum}></ItemPreview>
         </div>
       );
     }
   }
 
-  return (
-    <Accordion title={"Educational experience"}>{childrenElements()}</Accordion>
-  );
+  function children() {
+    console.log(items);
+    const listItems = items.map((item, index) => {
+      return getItem(item, index);
+    });
+
+    return (
+      <div className="accordion-content__wrapper">
+        {listItems}
+        <button type="button" className="add-more" onClick={handleAddMore}>
+          Add more
+        </button>
+      </div>
+    );
+  }
+
+  return <Accordion title={"Educational experience"}>{children()}</Accordion>;
 }
